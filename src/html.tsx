52b263683f38e1a0be944462d0671f688d27aeaa
f0reachARR <f0reach@f0reach.me>
2024-02-19 02:02:14 +0900

    Update url

--- src/html.tsx
+++ src/html.tsx
@@ -57,7 +57,7 @@ export const Credentials: FC<{ username: string; password: string }> = ({
           {`sudo curl -fsSL https://apt.fortefibre.net/fortefibre.asc -o /etc/apt/keyrings/fortefibre.asc
 
 echo \\
-  "deb [signed-by=/etc/apt/keyrings/fortefibre.asc] https://apt.fortefibre.net/repo/ jammy main" | \\
+  "deb [signed-by=/etc/apt/keyrings/fortefibre.asc] https://apt.fortefibre.net/ jammy main" | \\
   sudo tee /etc/apt/sources.list.d/fortefibre.list > /dev/null
 
 echo machine apt.fortefibre.net login ${username} password ${password} > /etc/apt/auth.conf.d/fortefibre.conf
