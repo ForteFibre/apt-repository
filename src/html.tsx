b5558cfdfa240084012cf12036ae19383e5260ab
f0reachARR <f0reach@f0reach.me>
2024-02-23 01:24:17 +0900

    Add rosdep code

--- src/html.tsx
+++ src/html.tsx
@@ -56,6 +56,8 @@ export const Credentials: FC<{ username: string; password: string }> = ({
         <code>
           {`sudo curl -fsSL https://apt.fortefibre.net/fortefibre.asc -o /etc/apt/keyrings/fortefibre.asc
 
+echo "yaml https://apt.fortefibre.net/rosdep/jammy/humble/rosdep.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/40-fortefibre-humble.list
+
 echo \\
   "deb [signed-by=/etc/apt/keyrings/fortefibre.asc] https://apt.fortefibre.net/ jammy main" | \\
   sudo tee /etc/apt/sources.list.d/fortefibre.list > /dev/null
